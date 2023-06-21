a47a7d77f0a81ae6508c08e0c3170e57ccf3d756
WhatIsFun <mohammed110051@gmail.com>
2023-06-21 22:55:06 +0400

    Task 1: IVehicle

---
+++ IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_interface
+{
+    internal interface IVehicle
+    {
+        
+        void Start();
+        void Accelerate(int speed);
+        void Brake();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace cSharp_interface
 {
@@ -6,17 +7,37 @@ namespace cSharp_interface
     {
         static void Main(string[] args)
         {
+            //IShape Example
             Circle crc = new Circle(5);
             Console.WriteLine($"The area of the circle is: {crc.CalculateArea()}, and the perimeter is: {crc.CalculaterPerimter()}");
             Console.WriteLine("_______________________________________________");
-
+             
             Rectangle rec = new Rectangle(4, 6);
             Console.WriteLine($"The area of the rectangle is: {rec.CalculateArea()}, and the perimeter is: {rec.CalculaterPerimter()}");
+
+            /* Task 1: IVehicle 
+             * Create an interface IVehicle with the following methods:
+
+                void Start()
+                void Accelerate(int speed)
+                void Brake()
+                Create two classes Car and Motorcycle that implement the IVehicle interface. 
+                Implement the methods according to the behavior of each vehicle type.
+             */
+            Car car = new Car();
+            Motorcycle motorcycle = new Motorcycle();
+            car.Start();
+            car.Accelerate(120);
+            car.Brake();
+            
+            motorcycle.Start();
+            motorcycle.Accelerate(60);
+            motorcycle.Brake();
 
         }
     }
 
-    public class Circle : IShape
+    public class Circle : IShape   // Circle class (Child class)
     {
         private double radius;
 
@@ -40,7 +61,7 @@ namespace cSharp_interface
         private double l;
         private double w;
 
-        public Rectangle(double l, double w)
+        public Rectangle(double l, double w) // Rectangle class (Child class)
         {
             this.l = l;
             this.w = w;
@@ -55,4 +76,45 @@ namespace cSharp_interface
             return 2*(l*w);
         }
     }
+    public class Car : IVehicle // Car class (Child class)
+    {
+        private int currentSpeed;
+         
+        public void Accelerate(int speed)
+        {
+            currentSpeed = speed;
+            Console.WriteLine($"The car is accelerating {currentSpeed} km/h");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("The car has stopped.");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("The car has started .");
+        }
+    }
+
+    public class Motorcycle : IVehicle  // Motorcycle class (Child class)
+    {
+        private int currentSpeed;
+        public void Accelerate(int speed)
+        {
+            currentSpeed = speed;
+            Console.WriteLine($"The car is accelerating {currentSpeed} km/h");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("The car has stopped.");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("The car has started .");
+        }
+    }
+
 }
